db83fffd60bc00ef13a7b7f7fc662add2c4e9928
unknown <teke156605@gmail.com>
2023-03-19 16:01:33 -0500

    Commit message

--- server.js
+++ server.js
@@ -1,5 +1,21 @@
 const express = require("express");
-const app = express();
+
+
+const app=express()
+const flash = require('express-flash');
+const session = require('express-session');
+const router=require("./routes/users")
+const cookieParser=require("cookie-parser")
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+  }));
+  
+  app.use(flash());
+ 
+
+const ejs=require("ejs")
 //var ejs = require("ejs");
 const path = require("path");
 app.use(express.static('public'));
@@ -7,7 +23,8 @@ app.use(express.static('public'));
 
 
 
-const cookieParser = require("cookie-parser");
+
+
 
 //const homeRoute = require('./controller.js')
 var testRoute = require("./controller/controller");
@@ -17,13 +34,26 @@ var homeRoute = require("./controller/homeController");
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
-app.listen(3000,(req,res,next)=>{
-    console.log("server is running")
-})
-
+app.set("view engine","html")
+app.engine("html",ejs.renderFile)
+app.set('views','./views')
+app.use(express.static('public'))
+app.use('/css',express.static(__dirname+'public/css'))
+app.use('/js',express.static(__dirname+'public/js'))
+app.use('/img',express.static(__dirname+'public/img'))
+app.use("/",router)
+app.use("/users/register",router)
+app.use("/users/login",router)
+app.use("/users/dashboard",router)
+app.use("/users/logout",router)
 
 
 app.use(testRoute);
 app.use(homeRoute);
+
+
+app.listen(2000,(req,res,next)=>{
+    console.log("server is running")
+})
+
 
